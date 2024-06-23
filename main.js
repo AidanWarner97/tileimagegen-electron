0e9ee4b471be39ed2c47bba8ff95ac8b9d2e1a96
Aidan Warner <aidan@awarner.co.uk>
2024-06-23 08:02:09 +0100

    v2.0.0: Offline support

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { create } = require('domain');
 const { app, BrowserWindow, session, ipcMain, shell} = require('electron');
 const { autoUpdater } = require('electron-updater');
+const { spawn } = require('child_process');
 const path = require('path');
 
 let mainWindow;
 let downloadItem;
+let hasReloaded = false;
 
 autoUpdater.logger = require('electron-log');
 autoUpdater.logger.transports.file.level = 'info';
@@ -27,7 +29,18 @@ function createWindow() {
         autoHideMenuBar: true
     });
 
-    mainWindow.loadURL('https://tileimagegen.uk');
+    mainWindow.loadURL('http://localhost:5000');
+
+    mainWindow.webContents.on('did-finish-load', () => {
+        if (!hasReloaded) {
+            hasReloaded = true;
+            mainWindow.webContents.reload();
+        }
+    });
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 
     session.defaultSession.on('will-download', (event, item, webContents) => {
         const filePath = path.join(app.getPath('downloads'), item.getFilename());
@@ -64,33 +77,61 @@ autoUpdater.on('error', (error) => {
 
 autoUpdater.on('checking-for-update', () => {
     console.log('Checking for update...');
-  });
+});
   
-  autoUpdater.on('update-available', (info) => {
+autoUpdater.on('update-available', (info) => {
     console.log('Update available:', info.version);
-  });
-  
-  autoUpdater.on('update-not-available', () => {
+});
+
+autoUpdater.on('update-not-available', () => {
     console.log('Update not available.');
-  });
+});
   
-  autoUpdater.on('update-downloaded', (info) => {
+autoUpdater.on('update-downloaded', (info) => {
     console.log('Update downloaded:', info.version);
-  });
-  
+});
 
 app.on('ready', () => {
+    const basePath = path.resolve(process.resourcesPath, '..');
+    const pythonExecutable = path.join(basePath, 'python_env', 'python.exe');
+    const flaskAppPath = path.join(basePath, 'python_app', 'wsgi.py');
+    flaskProcess = spawn(pythonExecutable, [flaskAppPath]);
+
+    flaskProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    flaskProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    flaskProcess.on('close', (code) => {
+        console.log(`Flask process exited with code ${code}`);
+    });
+    
     createWindow();
     autoUpdater.checkForUpdatesAndNotify();
 });
 
 app.on('window-all-closed', () => {
+    console.log('All windows closed. Stopping Flask process.');
+    if (flaskProcess) {
+        flaskProcess.kill();
+    }
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
+app.on('before-quit', () => {
+    console.log('App quitting. Stopping Flask process.');
+    if (flaskProcess) {
+        flaskProcess.kill();
+    }
+});
+
 app.on('activate', () => {
+    console.log('App activated.');
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
